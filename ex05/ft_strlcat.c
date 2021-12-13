d0791688f770757508be585a197050f745db1e1a
giorgiomiserendino <93911490+giorgiomiserendino@users.noreply.github.com>
2021-12-13 19:50:52 +0100

    Add files via upload

---
+++ ex05/ft_strlcat.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmiseren <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/08 17:29:59 by gmiseren          #+#    #+#             */
+/*   Updated: 2021/12/08 17:30:03 by gmiseren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	d_len;
+	unsigned int	s_len;
+	unsigned int	offset;
+	unsigned int	c;
+
+	d_len = ft_strlen (dest);
+	s_len = ft_strlen (src);
+	offset = d_len;
+	c = 0;
+	if (size == 0)
+		return (s_len);
+	while (src[c] != '\0' && offset + c < (size - 1))
+	{
+		dest[offset + c] = src[c];
+		c++;
+	}
+	if (c < size)
+		dest[offset + c] = '\0';
+	if (d_len > size)
+		return (s_len + size);
+	return (d_len + s_len);
+}
